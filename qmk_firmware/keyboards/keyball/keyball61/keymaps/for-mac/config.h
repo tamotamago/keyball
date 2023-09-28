c2e0060c19ebb2569326910f20def4cf5b94e0b6
tamotamago <denden_k_t_810@yahoo.co.jp>
2023-09-28 23:34:40 +0900

    Shorten tapping terms

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/for-mac/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/for-mac/config.h
@@ -34,3 +34,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 #define TAP_CODE_DELAY 5
+
+// Tapping terms
+// #define HOLD_ON_OTHER_KEY_PRESS
+#define TAPPING_TERM 100
