396e77b982fd9b60d8d9010dba40c0c0e0008f59
tamotamago <denden_k_t_810@yahoo.co.jp>
2023-10-02 00:08:07 +0900

    Changed AUTO_MOUSE_TIME

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/for-mac/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/for-mac/config.h
@@ -40,5 +40,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM 100
 
 // Automatic Mouse Layer
+// See: https://github.com/qmk/qmk_firmware/blob/master/docs/feature_pointing_device.md#how-to-customize
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 2
+#define AUTO_MOUSE_TIME 400
